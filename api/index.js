b8ca553dea950ae34519b568a32510736af18b26
Goutam Gurjar <gurjargoutam9982@gmail.com>
2024-09-04 17:54:54 +0530

    about added

--- api/index.js
+++ api/index.js
@@ -7,7 +7,9 @@ app.get('/',(req,res)=>{
      res.send("hello from goutam gurjar");
 })
 
-
+app.get('/about',(req,res)=>{
+     res.send("about page");
+})
 app.listen(8000,()=>{
      console.log('server is running on port 8000');
 })
