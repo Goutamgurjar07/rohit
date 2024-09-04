dfde7902a706c85a3c7c3df540f4049ad613b8ee
Goutam Gurjar <gurjargoutam9982@gmail.com>
2024-09-04 17:50:58 +0530

    cors added

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const express=require('express')
-
+const cors=require('cors')
 const app=express()
 
+app.use(cors())
 app.get('/',(req,res)=>{
      res.send("hello from goutam gurjar");
 })
 
 
-
-
 app.listen(8000,()=>{
      console.log('server is running on port 8000');
 })
+
